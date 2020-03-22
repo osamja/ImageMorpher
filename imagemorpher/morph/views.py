59bad0a01a3452d895115b2aaf5ded5b88752a3a
Sammy Javed <sammyjaved@gmail.com>
2020-03-22 02:08:58 +0000

    fix logging; add support for PNGs

--- imagemorpher/morph/views.py
+++ imagemorpher/morph/views.py
@@ -10,6 +10,9 @@ from django.http import JsonResponse
 from django.core import serializers
 from django.conf import settings
 from django.utils.html import escape
+
+import datetime
+from pytz import timezone
 import json
 import pdb
 import skimage.io as skio
@@ -17,9 +20,15 @@ from .morph import morph
 import base64
 import io
 from PIL import Image
-import logging
+import sys
+# morph is essentially the src root directory in this file now
+#   aka import all files with morph/<file-path>
+sys.path.insert(0, '/app/imagemorpher/morph')
 
-logging.basicConfig(filename='../logs/morph-app.log', format='%(name)s - %(levelname)s - %(message)s')
+import logging
+logger = logging.getLogger(__name__)
+
+# logging.basicConfig(filename='morph/logs/morph-app.log', level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
 
 def isRequestValid(request, Authorization='ImageMorpherV1'):
     try:
@@ -35,16 +44,25 @@ def isRequestValid(request, Authorization='ImageMorpherV1'):
 
 @api_view(["POST"])
 def index(request):
+    # pdb.set_trace()
     if not isRequestValid(request):
-        logging.error('request is not valid')
+        logging.info('request is not valid')
         return HttpResponse('Unauthorized', status=401)
     formData = request.FILES
+    # pdb.set_trace()
     img1 = skio.imread(formData['Image-1'])
     img2 = skio.imread(formData['Image-2'])
+
+    # In case img is a PNG with a 4th transparency layer, remove this layer
+    img1 = img1[:, :, :3]
+    img2 = img2[:, :, :3] 
+
     # img1 = skio.imread('/home/sammy/development/ImageMorpher/imagemorpher/morph/images/obama_small.jpg')
     # img2 = skio.imread('/home/sammy/development/ImageMorpher/imagemorpher/morph/images/george_small.jpg')
     
     try:
+        log_message = str(datetime.datetime.now(timezone('UTC'))) + ': Morphing images' 
+        logging.info(log_message)
         morphed_img_uri = morph(img1, img2, 0.5)
         return Response(morphed_img_uri)
     except Exception as e:
