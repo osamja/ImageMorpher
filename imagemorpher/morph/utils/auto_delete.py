54a6624657378cf525ebbd12013ce6ace97de136
Sammy Javed <sammyjaved@gmail.com>
2023-05-02 03:11:51 +0000

    add try catch in auto delete script

--- imagemorpher/morph/utils/auto_delete.py
+++ imagemorpher/morph/utils/auto_delete.py
@@ -4,7 +4,7 @@
 # CRON tab that runs this script every 24 hours
 # crontab -e (for initiating crontab file)
 # crontab -l for viewing cron tasks
-# 0 0 * * * /usr/bin/python /home/sammy/ImageMorpher/imagemorpher/morph/utils/auto_delete.py >> /home/sammy/ImageMorpher/imagemorpher/morph/logs/auto-delete-cron.log 2>&1
+# 0 0 * * * /usr/bin/python /app/imagemorpher/morph/utils/auto_delete.py >> /app/imagemorpher/morph/logs/auto-delete-cron.log 2>&1
 
 """
 Algorithm:
@@ -21,7 +21,7 @@ def get_date_x_days_ago(x_days_ago, date_from=None):
     return datetime.strptime(date_from, '%Y-%m-%d').date() - timedelta(x_days_ago) if date_from else date.today()-timedelta(x_days_ago)
 
 # change to directory where morphed images are stored
-path_of_saved_morphs = '/home/sammy/ImageMorpher/imagemorpher/morph/content/temp_morphed_images'
+path_of_saved_morphs = '/app/imagemorpher/morph/content/temp_morphed_images'
 os.chdir(path_of_saved_morphs)
 
 # get list of all filenames
@@ -30,12 +30,14 @@ morphed_filenames = os.listdir(path_of_saved_morphs)
 # get current year-month
 today = time.strftime("%Y-%m-%d")
 
-# get date 30 days ago
+# get date 3 days ago
 previous_date = datetime.strftime(get_date_x_days_ago(x_days_ago=3, date_from=today), "%Y-%m-%d")
 
 for morphed_filename in morphed_filenames:
-    morphed_file_date = datetime.strptime(morphed_filename[:10], "%Y-%m-%d")
-    previous_date_obj = datetime.strptime(previous_date, "%Y-%m-%d")
-    
-    if morphed_file_date < previous_date_obj:
-        os.remove(morphed_filename)
+    try:
+        morphed_file_date = datetime.strptime(morphed_filename[:10], "%Y-%m-%d")
+        previous_date_obj = datetime.strptime(previous_date, "%Y-%m-%d")
+        if morphed_file_date < previous_date_obj:
+            os.remove(morphed_filename)
+    except ValueError:
+        continue
